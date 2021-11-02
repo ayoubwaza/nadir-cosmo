f35efb205b537cc7446281d07c5029e6eac4c3c9
Your Name <you@example.com>
2021-11-02 14:05:36 +0100

    adding autoplay property to swiper

--- components/sliders_Img.js
+++ components/sliders_Img.js
@@ -1,32 +1,66 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/swiper-react.cjs.js";
-
 import SwiperCore, {
-  EffectFade,
   Autoplay,
   Navigation,
   Pagination,
-} from "swiper/core";
-SwiperCore.use([EffectFade, Autoplay, Navigation, Pagination]);
+  Mousewheel,
+  Keyboard,
+} from "swiper";
+import styles from "../styles/Main.module.scss";
+SwiperCore.use([Autoplay, Navigation, Pagination, Mousewheel, Keyboard]);
+
 export default function SLiderImg() {
   return (
     <>
       <Swiper
-        direction={"vertical"}
-        pagination={{
-          clickable: true,
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
         }}
+        aut
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className={styles.each_slider}></div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
