fd6caff305986439bc19549faf6857b207d79986
Your Name <you@example.com>
2021-11-07 22:29:19 +0100

    chnage colors

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,6 @@ import React, { useEffect, useRef } from "react";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
 import styles from "../styles/Main.module.scss";
-import Image from "next/image";
 function About() {
   return (
     <>
@@ -22,10 +21,9 @@ function About() {
       >
         <div className={styles.__about_child}>
           <div>
-            <h1>brightness</h1>
+            <h3>brightness</h3>
           </div>
           <div className={styles.__about_child_content}>
-            <div>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 <br />
@@ -35,10 +33,6 @@ function About() {
                 <br />
                 iure aspernatur illo!
               </p>
-            </div>
-            <div>
-              <Image src="/about-img1.jpg" width="340" height="460" alt="" />
-            </div>
           </div>
         </div>
       </motion.div>
